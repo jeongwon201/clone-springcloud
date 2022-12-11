b0250aaf0664832c772578ea99d7aa5de3166c37
jeongwon <jeongwon201@naver.com>
2022-12-11 18:27:29 +0900

    local sh update

--- run/local/001.env_set.sh
+++ run/local/001.env_set.sh
@@ -1,5 +1,4 @@
 sudo docker run -d --rm --name mysqlDB \
---net apps_net \
 -e MYSQL_DATABASE=review-db \
 -e MYSQL_USER=user01 \
 -e MYSQL_PASSWORD=user01 \
@@ -8,7 +7,6 @@ sudo docker run -d --rm --name mysqlDB \
 mysql:5.7
 
 sudo docker run -d --rm --name mongoDB \
---net apps_net \
 -p 27017:27017 \
 mongo:3.6.9 \
 mongod --smallfiles
