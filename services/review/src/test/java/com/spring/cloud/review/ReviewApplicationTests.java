ef630fea0c5c20f5cb9db9dfee5e951711a4d6e0
jeongwon <jeongwon201@naver.com>
2022-12-13 11:09:14 +0900

    review test application error fix

--- services/review/src/test/java/com/spring/cloud/review/ReviewApplicationTests.java
+++ services/review/src/test/java/com/spring/cloud/review/ReviewApplicationTests.java
@@ -3,7 +3,7 @@ package com.spring.cloud.review;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"spring.datasource.url=jdbc:h2:mem:review-db"})
+@SpringBootTest
 class ReviewApplicationTests {
 
     @Test
