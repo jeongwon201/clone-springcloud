ba174a38b98aa3ac01bbae31d69163f525ede332
jeongwon <jeongwon201@naver.com>
2022-12-13 12:04:32 +0900

    add (run with docke)r

---
+++ run/docker/004.delete_docker_container.sh
@@ -0,0 +1,22 @@
+sudo docker stop composite;
+
+sudo docker stop product;
+
+sudo docker stop recommend;
+
+sudo docker stop review;
+
+sudo docker stop mongoDB;
+
+sudo docker stop mysqlDB;
+
+
+sudo docker rmi composite;
+
+sudo docker rmi product;
+
+sudo docker rmi recommend;
+
+sudo docker rmi review;
+
+sudo docker network rm apps_net;
